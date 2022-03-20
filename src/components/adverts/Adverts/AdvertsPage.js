055ce0bc5d7605cd34df530f4ee9d0ee3a1efff7
Jose Antonio Cascales <jose.cascalesbo@gmail.com>
2022-03-20 10:26:36 +0100

    creacion advertspage

---
+++ src/components/adverts/Adverts/AdvertsPage.js
@@ -0,0 +1,8 @@
+const AdvertsPage = () => (<div className="advertsPage">
+    <ul>
+        <li>Primer anuncio</li>
+        <li>Segundo anuncio</li>
+    </ul>
+</div>
+);
+export default AdvertsPage;
