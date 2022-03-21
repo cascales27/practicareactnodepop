2ae0c94f33600e4413016945fa267e95c9474401
Jose Antonio Cascales <jose.cascalesbo@gmail.com>
2022-03-21 21:28:48 +0100

    formulario 1.0

--- src/components/auth/LoginPage/LoginPage.js
+++ src/components/auth/LoginPage/LoginPage.js
@@ -5,38 +5,59 @@ import Button from "../../common/Button";
 function LoginPage() {
 
     const [credentials, setCredentials] = useState({
-        username: '',
-         password: '',
-        });
+        email: '',
+        password: '',
+        remember: false,
+    });
 
-        const handleChange = event => {
-            setCredentials(credentials => ({
-                ...credentials,
-                [event.target.name]: event.target.value,
-            }));
-        };
+    const { email, password, remember } = credentials;
+
+    const handleChange = event => {
+        setCredentials(credentials => ({
+            ...credentials,
+            [event.target.name]:
+             event.target.type === 'checkbox'
+              ? event.target.checked
+              : event.target.value,
+        }));
+    };
+
+    const handlesubmit = (event) => {
+        event.preventDefault();
+    }
     return (
 
-    <div className="loginPage">
-        <h1 className="loginPage-title">Entra en la tienda</h1>
-        <form>
-            <input 
-            type="text"
-            name="username" 
-            value={credentials.username}
-             onChange={handleChange}
-            
-            />
-            <input
-             type="password" 
-            name="pasword"
-             value={credentials.password}
-            onChange={handleChange}>
-                </input>
-            <Button type="submit" variant="primary" > Entra</Button>
-        </form>
-    </div>
-     );
+        <div className="loginPage">
+            <h1 className="loginPage-title">Entra en la tienda</h1>
+            <form onSubmit={handlesubmit}>
+                <input
+                    type="text"
+                    name="email"
+                    value={email}
+                    onChange={handleChange}
+
+                />
+                <input
+                    type="password"
+                    name="password"
+                    value={password}
+                    onChange={handleChange}
+                />
+
+                <input
+                    type="checkbox"
+                    name="remember"
+                    checked={remember}
+                    onChange={handleChange}
+                ></input>
+                <Button
+                    type="submit"
+                    variant="primary"
+                    disabled={!email || !password}>
+                    Entra</Button>
+            </form>
+        </div>
+    );
 }
 
 export default LoginPage;
